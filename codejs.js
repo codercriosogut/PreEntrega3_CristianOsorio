a7dbf714892d0c889607cae499f6e7654269e83d
Cristian Osorio Gutiérrez <cosorio@hotmail.cl>
2023-10-01 23:07:54 -0300

    up
    up

--- codejs.js
+++ codejs.js
@@ -1,153 +1,192 @@
-class Plan{
-    constructor(plan){
-        this.type = plan.type;
-        this.price = plan.price;
-        this.description = plan.description;
-    }
-    assemble(){
-        console.log(this.type)
-        return ` <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">${this.type}</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">$${this.price}</h1>
-                    <p><p><p><p>
-                    ${this.description}
-                    </p>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary" onclick="buyPlan('${this.type}', '${this.price}', '${this.description}')">Comprar</button>
-                </div>`;
-                
-    }
+
+
+
+class Item{
+  constructor(item){
+    this.nombre = item.nombre;
+    this.precio = item.precio;
+    this.descripcion = item.descripcion;
+  }
+  assemble(){
+    console.log(this.nombre)
+    return `<div class="card-header py-3">
+            <h4 class="my-0 fw-normal">${this.nombre}</h4>
+            </div>
+            <div class="card-body">
+            <h1 class="card-title pricing-card-title">$${this.precio}</h1>
+            <p><p><p><p>
+            ${this.descripcion}
+            </p>
+            <button nombre="button" id="eventoCompra" class="w-100 btn btn-lg btn-outline-primary" onclick="buyPlan('${this.nombre}', '${this.precio}', '${this.descripcion}')">Comprar</button>
+            </div>`;
+  }
 }
-// Datos de la compra del plan
-let message = document.getElementById("message");
+
+
+//error
+/*
+let eventoCompra = document.getElementById("eventoCompra");
+eventoCompra.addEventListener('click', () => {
+  Toastify({
+    text: "This is a toast",
+    duration: 3000,
+    gravity: 'bottom',
+    position: 'left'
+  }).showToast();
+})
+*/
+
+
+
+// Datos de la compra del item
+let mensaje = document.getElementById("mensaje");
 // formulario
-let formPlan = document.getElementById("formPlan");
-// Seccion de todos los planes
-let allPlans = document.getElementById("allPlans");
-formPlan.addEventListener("submit",createPlan);
+let FormProduc = document.getElementById("FormProduc");
+// Seccion de todos los productos
+let totalProduc = document.getElementById("totalProduc");
+FormProduc.addEventListener("submit",crearProduc);
 // Variable Global
-let planList;
+let listaProduc;
 let stotageList;
 // falsy
-planList = JSON.parse(localStorage.getItem("planList")) || [];
-const cartItems = document.getElementById("cartItems");
+listaProduc = JSON.parse(localStorage.getItem("listaProduc")) || [];
+const cards = document.getElementById("cards");
 const checkoutButton = document.getElementById("checkoutButton");
 
-function buyPlan(type, price, description) {
-  const cartItem = document.createElement("li");
-  cartItem.innerHTML = `${type} - $${price}`;
-  cartItems.appendChild(cartItem);
 
-  // Guardar la información en una estructura de datos
-  // Puedes usar un array para mantener un seguimiento de los elementos en el carrito
-  // Por ejemplo:
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push({ type, price });
-  localStorage.setItem("cart", JSON.stringify(cart));
-  message.append(messageBody);
-  message.className = "alert alert-success visible";
+function buyPlan(nombre, precio, descripcion) {
+  const cartItem = document.createElement("li");
+  cartItem.innerHTML = `${nombre} - $${precio}`;
+  cards.appendChild(cartItem);
+  //guardar informacion a la estructura de datos
+  const comprasUsers = JSON.parse(localStorage.getItem("comprasUsers")) || [];
+  comprasUsers.push({ nombre, precio });
+  localStorage.setItem("comprasUsers", JSON.stringify(comprasUsers));
+  mensaje.append(messageBody);
+  mensaje.className = "alert alert-success visible";
 }
 
+
+
+//ARREGLADO
 checkoutButton.addEventListener("click", () => {
   // Implementa la lógica para procesar el pedido aquí
   // Puedes acceder a los elementos del carrito desde localStorage
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
-  if (cart.length === 0) {
-//alert("El carrito está vacío. Agrega planes antes de realizar el pedido.");
-
-
-
-//alerta sweetalert
-Swal.fire({
-  icon: 'error',
-  title: '¡Error!',
-  text: 'El carrito esta vacío!',
-  footer: '<a>Favor agrega nuevos productos al carro de compras.</a>'
-})
+  const comprasUsers = JSON.parse(localStorage.getItem("comprasUsers")) || [];
+  if (comprasUsers.length === 0) {
+    //alert("El carrito está vacío. Agrega planes antes de realizar el pedido.");
+    //alerta sweetalert
+    Swal.fire( {
+      icon: 'error',
+      title: '¡Error!',
+      text: 'El carrito esta vacío!',
+      footer: '<a>Favor agrega nuevos productos al carro de compras.</a>'
+    })
   } else {
+    localStorage.removeItem("comprasUsers");
+    cards.innerHTML = "";
+    //alerta sweetalert
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'El pedido se ha realizado con éxito!',
+      showConfirmButton: false,
+      timer: 2000
+    })
+    // Realiza la lógica para procesar el pedido, por ejemplo, enviar los datos al servidor, etc.
+    //    alert("Pedido realizado con éxito");
+    // Limpia el carrito después de realizar el pedido
 
-
-//alerta sweetalert
-Swal.fire({
-  position: 'center',
-  icon: 'success',
-  title: 'El pedido se ha realizado con éxito!',
-  showConfirmButton: false,
-  timer: 5000
-})
-// Realiza la lógica para procesar el pedido, por ejemplo, enviar los datos al servidor, etc.
-//    alert("Pedido realizado con éxito");
-// Limpia el carrito después de realizar el pedido
-    localStorage.removeItem("cart");
-    cartItems.innerHTML = "";
   }
 });
 
-const showPlans = ()=>{
-    let stotaged = planList;
-    stotageList = [];
-    allPlans.innerHTML = "";
 
-    for(const objeto of stotaged){
-        console.log(objeto);
-        stotageList.push(new Plan(objeto));
-    }
-    console.log(stotageList);
-
-    for(const plan of stotageList){
-        let firstDiv = document.createElement("div");
-        firstDiv.className="col";
-        let secondDiv = document.createElement("div");
-        secondDiv.className ="card mb-4 rounded-3 shadow-sm";
-        /*secondDiv.innerHTML =` <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">${plan.type}</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">$${plan.price}<small class="text-muted fw-light">/mo</small></h1>
-                    <p>
-                    ${plan.description}
-                    </p>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">Comprar Plan</button>
-                </div>`;*/
-        secondDiv.innerHTML = plan.assemble();
-        firstDiv.append(secondDiv);
-        allPlans.append(firstDiv)
-    }
-  
+const verProductos = ()=>{
+  let stotaged = listaProduc;
+  stotageList = [];
+  totalProduc.innerHTML = "";
+  for(const objeto of stotaged){
+    console.log(objeto);
+    stotageList.push(new Item(objeto));
+  }
+  console.log(stotageList);
+  for(const item of stotageList){
+    let firstDiv = document.createElement("div");
+    firstDiv.className="col";
+    let secondDiv = document.createElement("div");
+    secondDiv.className ="card mb-4 rounded-3 shadow-sm";
+    secondDiv.innerHTML = item.assemble();
+    firstDiv.append(secondDiv);
+    totalProduc.append(firstDiv)
+  }
 }
-
+//agrega nuevo item al storage
 /**
- * Agrega un nuevo plan al Storage
- * @param {Object} plan 
- */
-const newPlan = (plan) =>{
-   //console.log(plan)
-   planList.push(plan);
-   //console.log(planList)
-   localStorage.setItem("planList", JSON.stringify(planList));
-   showPlans();
-}
+*@param {Object} item 
+*/
 
+
+const nuevoProduc = (item) =>{
+  //console.log(item)
+  listaProduc.push(item);
+  //console.log(listaProduc)
+  localStorage.setItem("listaProduc", JSON.stringify(listaProduc));
+  verProductos();
+  //borrar contenido al agregar nuevo producto al inventario
+  tipoProduc.value = "";
+  tipoPrecio.value = "";
+  tipoDescripcion.value = "";
+  //Mensaje producto guardado
+  Swal.fire({
+    title: '¡Producto guardado con Éxito!',
+    text: 'Los campos llenos se borrarán',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
+}
 /**
  * Recibe los datos del formulario para la creación 
- * del nuevo Plan.
+ * del nuevo Item.
  * @param {Object} e 
  */
-function createPlan(e){
-   // Inputs
-    let planType = document.getElementById("planType").value;
-    let planPrice = document.getElementById("planPrice").value;
-    let planDescription = document.getElementById("planDescription").value;
 
-    e.preventDefault();
 
-    let plan = {
-        type: planType,
-        price: planPrice,
-        description: planDescription
-    }
-
-    newPlan(plan);
+function crearProduc(e){
+  // Inputs
+  let tipoProduc = document.getElementById("tipoProduc").value;
+  let tipoPrecio = document.getElementById("tipoPrecio").value;
+  let tipoDescripcion = document.getElementById("tipoDescripcion").value;
+  e.preventDefault();
+  let item = {
+    nombre: tipoProduc,
+    precio: tipoPrecio,
+    descripcion: tipoDescripcion
+  }
+  nuevoProduc(item);
 }
-showPlans();
+
+// Función para borrar todo el contenido del localStorage
+function clearLocalStorage() {
+  localStorage.clear();
+  totalProduc.innerHTML = "";
+  location.reload();
+  // También puedes agregar un mensaje de confirmación si lo deseas
+  alert("El contenido del localStorage se ha borrado correctamente.");
+}
+// Puedes agregar un evento para llamar a esta función cuando sea necesario
+// Por ejemplo, puedes agregar un botón en tu HTML y vincularlo a esta función
+document.getElementById("clearLocalStorageButton").addEventListener("click", clearLocalStorage);
+
+/*
+const clearLocalStorageButton = document.getElementById("clearLocalStorageButton");
+clearLocalStorageButton.addEventListener("click", clearLocalStorage);
+*/
+
+
+
+
+verProductos();
